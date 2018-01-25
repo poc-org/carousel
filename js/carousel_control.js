be297f7994ce91be2374c0a2ce259436d181aa29
cadriosula <cfa_44@yahoo.com>
2018-01-25 21:07:40 +0800

    Create carousel_control.js

---
+++ js/carousel_control.js
@@ -0,0 +1,12 @@
+var control = '<div style="position:absolute;bottom:80px;left:130px;padding-left:0;width:500px;"> '
+controlText += '\n<a class="carousel-control" href="#myCarousel" data-slide="next">Next Feature</a> '
+controlText += '\n </div> '
+controlText += '\n<div style="position:absolute;bottom:16px;left:190px;padding-left:0;"> '
+controlText += '\n<svg width="100" height="100">'
+controlText += '\n<a class="carousel-control" href="#myCarousel" data-slide="next">'
+controlText += '\n<circle cx="45" cy="45" r="12" stroke="none" fill="yellow" />'
+controlText += '\n<polygon points="40,38 40,52 53,45" fill="#000"/>'
+controlText += '\n </a>'
+controlText += '\n</svg>'
+controlText += '\n</div> '
+document.write(controlText);
