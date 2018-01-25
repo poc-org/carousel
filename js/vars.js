a3bd21c84993cfeb5000de4cf907d9754f0fb2b8
cadriosula <cfa_44@yahoo.com>
2018-01-25 20:28:56 +0800

    Update vars.js

--- js/vars.js
+++ js/vars.js
@@ -1,4 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-    $scope.url_home = "https://rawgit.com/poc-org/hrit/test";
+    $scope.url_css_home = "https://rawgit.com/poc-org/carousel/master";
+    $scope.url_js_home = "https://raw.githubusercontent.com/poc-org/carousel/master";
 });
